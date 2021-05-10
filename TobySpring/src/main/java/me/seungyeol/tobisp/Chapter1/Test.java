bdd21a4d39b2ac37d03f8f6eedced95bd30b0540
백승열 <beck33333@naver.com>
2021-05-10 22:25:04 +0900

    feat : add proxy code

---
+++ TobySpring/src/main/java/me/seungyeol/tobisp/Chapter1/Test.java
@@ -0,0 +1,9 @@
+package me.seungyeol.tobisp.Chapter1;
+
+import java.io.*;
+
+public class Test {
+    public static void main(String[] args) throws FileNotFoundException {
+        InputStream ls = new BufferedInputStream(new FileInputStream("a.txt"));
+    }
+}
